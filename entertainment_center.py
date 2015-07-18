0bbaf6350e28e5eff240e23cd7d542bd094f2e3d
micahkim23 <micahkim23@gmail.com>
2015-07-17 22:44:16 -0700

    Fixed readability of code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,31 +1,40 @@
 import media
 import fresh_tomatoes
 
-#create 6 different instances of Movie objects to be used in webpage
-the_interview = media.Movie("The Interview",
-                            "Two TV hosts travel to North Korea to kill Kim Jong Un",
-                            "https://upload.wikimedia.org/wikipedia/en/2/27/The_Interview_2014_poster.jpg",
-                            "https://www.youtube.com/watch?v=KpyVENBPj5c")
-avengers = media.Movie("Avengers Age of Ultron",
-                     "The Avengers have to defeat Ultron",
-                     "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",
-                     "https://www.youtube.com/watch?v=JAUoeqvedMo")
-t1_jump_street = media.Movie("21 Jump Street",
-                             "Two undercover officers have to shut down a drug operation at a high school",
-                             "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/21JumpStreetfilm.jpg/220px-21JumpStreetfilm.jpg",
-                             "https://www.youtube.com/watch?v=RLoKtb4c4W0")
-inside_out = media.Movie("Inside Out", "Meet the voices inside a young girl's head",
-                             "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_(2015_film)_poster.jpg",
-                             "https://www.youtube.com/watch?v=seMwpP0yeu4")
-transformers_age_of_extinction = media.Movie("Transformers Age of Extinction", "The Autobots must defeat the Decepticons",
-                          "https://upload.wikimedia.org/wikipedia/en/9/94/Transformers_Age_of_Extinction_Poster.jpeg",
-                          "https://www.youtube.com/watch?v=pbI980iUb78")
-mockingjay = media.Movie("Mockingjay", "The capitol needs to be destroyed",
-                           "https://upload.wikimedia.org/wikipedia/en/6/63/MockingjayPart1Poster3.jpg",
-                           "https://www.youtube.com/watch?v=3PkkHsuMrho")
+# create 6 different instances of Movie objects to be used in webpage
+the_interview = media.Movie(
+    "The Interview",
+    "Two TV hosts travel to North Korea to kill Kim Jong Un",
+    "https://upload.wikimedia.org/wikipedia/en/2/27/The_Interview_2014_poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=KpyVENBPj5c")
+avengers = media.Movie(
+    "Avengers Age of Ultron",
+    "The Avengers have to defeat Ultron",
+    "https://upload.wikimedia.org/wikipedia/en/1/1b/Avengers_Age_of_Ultron.jpg",  # noqa
+    "https://www.youtube.com/watch?v=JAUoeqvedMo")
+t1_jump_street = media.Movie(
+    "21 Jump Street",
+    "Two undercover officers have to shut down a drug operation at a high school",  # noqa
+    "https://upload.wikimedia.org/wikipedia/en/thumb/9/93/21JumpStreetfilm.jpg/220px-21JumpStreetfilm.jpg",  # noqa
+    "https://www.youtube.com/watch?v=RLoKtb4c4W0")
+inside_out = media.Movie(
+    "Inside Out", 
+    "Meet the voices inside a young girl's head",
+    "https://upload.wikimedia.org/wikipedia/en/0/0a/Inside_Out_(2015_film)_poster.jpg",  # noqa
+    "https://www.youtube.com/watch?v=seMwpP0yeu4")
+transformers_age_of_extinction = media.Movie(
+    "Transformers Age of Extinction", 
+    "The Autobots must defeat the Decepticons",
+    "https://upload.wikimedia.org/wikipedia/en/9/94/Transformers_Age_of_Extinction_Poster.jpeg",  # noqa
+    "https://www.youtube.com/watch?v=pbI980iUb78")
+mockingjay = media.Movie(
+    "Mockingjay", 
+    "The capitol needs to be destroyed",
+    "https://upload.wikimedia.org/wikipedia/en/6/63/MockingjayPart1Poster3.jpg",  # noqa
+    "https://www.youtube.com/watch?v=3PkkHsuMrho")
 
-#array of movies
-movies = [the_interview, avengers, t1_jump_street, inside_out, transformers_age_of_extinction, mockingjay]
-#opens movie page with the array of movies
+# array of movies
+movies = [the_interview, avengers, t1_jump_street, inside_out, transformers_age_of_extinction, mockingjay]  # noqa
+# opens movie page with the array of movies
 fresh_tomatoes.open_movies_page(movies)
 
--- media.py
+++ media.py
@@ -1,10 +1,11 @@
 import webbrowser
 
-#class to store information about Movie 
+
+# class to store information about Movie 
 class Movie():
     """ This class provides a way to store movie related information"""
     
-    #initializes Movie object
+    # initializes Movie object
     def __init__(self, movie_title, movie_storyline, poster_image,
                  trailer_youtube):
         self.title = movie_title
@@ -12,6 +13,6 @@ class Movie():
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #method to show youtube trailer
+    # method to show youtube trailer
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
